e3e507d814c3bbe3585780028954d4f602c94482
CHRIS-DESKTOP\Chris <chris.simmons854@gmail.com>
2017-11-01 23:44:03 -0500

    wat

--- SharOnProtocal/src/mvn/app/Client.java
+++ SharOnProtocal/src/mvn/app/Client.java
@@ -16,10 +16,13 @@ import java.util.Random;
 import java.util.Scanner;
 import java.util.StringTokenizer;
 
-/**
- * Created by Chris on 10/31/2017.
- */
+
 public class Client {
+
+    /**
+     * @param args main program arguments args[0] is address args[1] is port number
+     * @throws IOException
+     */
     public static void main(String [] args) throws IOException {
         if ((args.length < 2) ) { // Test for correct # of args
             throw new IllegalArgumentException("Parameter(s): <Server Name/IP> <Port> ");
@@ -35,27 +38,34 @@ public class Client {
         DatagramSocket socket = new DatagramSocket();
         DatagramPacket udpPacket;
 
+        //Continue prompting user till command is exit
         while (!command.equals("exit")) {
             try {
                 packet = null;
+                //prompt user
                 System.out.print("Please Enter a Command: \n>");
                 Scanner reader = new Scanner(System.in);  // Reading from System.in
                 command = reader.nextLine();
                 StringTokenizer commandTokenizer = new StringTokenizer(command);
 
+                //check if a command was entered
                 if (!command.equals("")) {
                     cmdType = commandTokenizer.nextToken();
                 }
 
                 sessionID = r.nextInt(255 - 1) + 1;
 
+                //if command is RN send a RequestNodes Packet
                 if (cmdType.equals("RN")) {
                     packet = new Packet(PacketType.RequestNodes, ErrorType.None, sessionID);
                 }
+                //if command is RM send a RequestMavens Packet
                 if (cmdType.equals("RM")) {
                     packet = new Packet(PacketType.RequestMavens, ErrorType.None, sessionID);
                 }
+                //if command is NA send a NodeAdditions Packet
                 if (cmdType.equals("NA")) {
+                    //check for valid arguments
                     if (commandTokenizer.hasMoreTokens()) {
                         packet = new Packet(PacketType.NodeAdditions, ErrorType.None, sessionID);
                         while (commandTokenizer.hasMoreTokens()) {
@@ -65,7 +75,9 @@ public class Client {
                         System.err.println("NA Command Expects at least one argument");
                     }
                 }
+                //if command is MA send a MavenAdditions Packet
                 if (cmdType.equals("MA")) {
+                    //check for valid arguments
                     if (commandTokenizer.hasMoreTokens()) {
                         packet = new Packet(PacketType.MavenAdditions, ErrorType.None, sessionID);
                         while (commandTokenizer.hasMoreTokens()) {
@@ -75,7 +87,9 @@ public class Client {
                         System.err.println("MA Command Expects at least one argument");
                     }
                 }
+                //if command is ND send a NodeDeletions Packet
                 if (cmdType.equals("ND")) {
+                    //check for valid arguments
                     if (commandTokenizer.hasMoreTokens()) {
                         packet = new Packet(PacketType.NodeDeletions, ErrorType.None, sessionID);
                         while (commandTokenizer.hasMoreTokens()) {
@@ -85,7 +99,9 @@ public class Client {
                         System.err.println("ND Command Expects at least one argument");
                     }
                 }
+                //if command is MD send a MavenDeletions Packet
                 if (cmdType.equals("MD")) {
+                    //check for valid arguments
                     if (commandTokenizer.hasMoreTokens()) {
                         packet = new Packet(PacketType.MavenDeletions, ErrorType.None, sessionID);
                         while (commandTokenizer.hasMoreTokens()) {
@@ -96,16 +112,18 @@ public class Client {
                     }
                 }
 
+                //if a packet was created send it
                 if (packet != null) {
+                    //create buffer to send via UDP
                     byte[] packetByteArray = packet.encode();
+                    //create UDP packet
                     udpPacket = new DatagramPacket(packetByteArray, packetByteArray.length, serverAddress, serverPort);
                     int attempts = 0;
                     Boolean receivedFlag = false;
                     Packet newPacket = null;
-                    if(!(packet.getType() == PacketType.RequestMavens || packet.getType() == PacketType.RequestNodes)){
-                        socket.send(udpPacket);
-                    }
-                    else {
+                    //send Packet
+                    socket.send(udpPacket);
+                    if((packet.getType() == PacketType.RequestMavens || packet.getType() == PacketType.RequestNodes)){
                         try {
                             while (attempts < 3 && !receivedFlag) {
                                 socket.setSoTimeout(3000);
@@ -134,8 +152,8 @@ public class Client {
                                     attempts++;
                                 }
                             }
-                            if (!receivedFlag && newPacket.getType() == PacketType.RequestMavens ||
-                                    newPacket.getType() == PacketType.RequestNodes) {
+                            if (!receivedFlag && (packet.getType() == PacketType.RequestMavens ||
+                                    packet.getType() == PacketType.RequestNodes)) {
                                 System.err.println("No Response from Server");
                             }
                         } catch (IOException e) {
@@ -150,7 +168,12 @@ public class Client {
     }
 
 
-
+    /**
+     * Creates an InetSocketAddress from a string following this format:
+     * address:port
+     * @param address a string ins address:port format
+     * @return InetSocketAddress
+     */
     public static InetSocketAddress createAddress(String address){
         StringTokenizer addressTokenizer = new StringTokenizer(address);
         String newAddressIP = addressTokenizer.nextToken(":");
